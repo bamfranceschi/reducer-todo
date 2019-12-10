67246f64a1f5ea8f5e1ad14b346a0c57c69b0211
bamfranceschi <anna.franceschi@gmail.com>
2019-12-10 15:56:56 -0600

    created react app and first task complete

---
+++ src/reducers/reducer.js
@@ -0,0 +1,32 @@
+export const initialState = [
+    {
+    item: 'check email',
+    completed: false,
+    id: 1
+},
+{
+    item: 'buy Xmas tree',
+    completed: false,
+    id: 2
+},
+{
+    item: 'cook dinner',
+    completed: false,
+    id: 3
+},
+{
+    item: 'walk the dog',
+    completed: false,
+    id: 4
+},
+{
+    item: 'unpack',
+    completed: false,
+    id: 5
+}
+]
+
+export const reducer = (state, action) => {
+    default:
+        return state;
+}
